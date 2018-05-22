39736e7e49d8314068798fe6a8c6594af4d874c5
Lizaveta <lizastrapko@gmail.com>
2018-05-22 07:13:17 +0300

    Add main methods

--- Program/Program.cs
+++ Program/Program.cs
@@ -15,7 +15,57 @@ namespace Program
 
             string writePath = ConfigurationManager.AppSettings["writePath"];
 
-            
+            try
+            {
+                using (StreamReader sr = new StreamReader(filePath))
+                {
+                    string line = sr.ReadToEnd();
+
+                    StringBuilder f = new StringBuilder(line, 1000);
+
+                    Parser parser = new Parser();
+
+                    Texts text = parser.Parse(f);
+
+                    Console.WriteLine(f);
+
+                    Console.WriteLine("----------Sentences ordered by the number of words------------\n\n");
+
+                    foreach (var item in text.GetListOfSentByNumOfWord())
+                    {
+                        Console.WriteLine(item);
+                    }
+
+                    Console.WriteLine("---------Words by length from interrogative sentence-----------\n\n");
+
+                    foreach (var item in text.GetListWordsInSent(2, TypeSent.Voprositelnoe))
+                    {
+                        Console.WriteLine(item);
+                    }
+
+                    Console.WriteLine("\n\n");
+
+                    Console.WriteLine("------------Delete words starts with consonants--------------\n\n");
+
+                    text.DelWordsWithConsonants(3);
+
+                    Console.WriteLine(text);
+
+                    Console.WriteLine("------------------Replace word in sentence-------------------\n\n");
+
+                    text.ReplacementWordsInSent(1, 3, "It's a substring");
+
+                    Console.WriteLine(text);
+                }
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("The process failed: {0}", e);
+            }
+
+
+
             Console.ReadKey();
         }
        
